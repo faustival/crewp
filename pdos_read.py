c1f15c647d8eafeebf4a06fd61d04ee44b48a1f6
jinxi <jinxi@jinxi-pc>
2015-05-14 17:33:45 +0800

    finish read_pdos function

--- pdos_read.py
+++ pdos_read.py
@@ -1,12 +1,21 @@
 #! /usr/bin/python3
 
+import os
+
 workpath = '/home/jinxi/pwjobs/au_surface_proj/au111_lyr5/'
+os.chdir(workpath)
+inf = open('au111.pdos.plot.pdos_atm#1(Au)_wfc#3(d)', 'r')
 
-inf = 'au111.pdos.plot.pdos_atm#1(Au)_wfc#3(d)'
-
-print(inf,workpath)
-
-def read_pdosf(inpf,spin)
+def read_pdosf(inpf, spin_pol):
+    '''
+    input:
+      inpf:      input filename, 
+      spin_pol:  True for spin-polarized
+    output:
+      pdos:      
+    '''
+    pdos = []
+    print('initial pdos', pdos)
     while True:
         line = inpf.readline()
         if not line:
@@ -14,7 +23,18 @@ def read_pdosf(inpf,spin)
         words = line.split()
         if words[0]=='#':
             continue
-        return pdos
+        else:
+            if not spin_pol:
+                pdos.append( float(words[1]) )
+            elif spin_pol:
+                pdos.append( [float(words[1]),float(words[2])] )
+    return pdos
+
+
+
+pdos = read_pdosf(inf, True)
+
+print(pdos)
 
 
 
