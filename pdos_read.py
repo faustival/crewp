c95e2ebfc255eb9fe10baa0f085a9f7f80791e15
anyonsites <xjin.xmu@gmail.com>
2015-05-15 08:39:16 +0800

    finish pdos reading

--- pdos_read.py
+++ pdos_read.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import os
+import sys
 
 workpath = '/home/jinxi/pwjobs/au_surface_proj/au111_lyr5/'
 os.chdir(workpath)
@@ -8,14 +9,20 @@ inf = open('au111.pdos.plot.pdos_atm#1(Au)_wfc#3(d)', 'r')
 
 def read_pdosf(inpf, spin_pol):
     '''
-    input:
-      inpf:      input filename, 
-      spin_pol:  True for spin-polarized
-    output:
-      pdos:      
+    INPUT:
+        inpf:      input filename, 
+        spin_pol:  True for spin-polarized
+    OUTPUT:
+        engy:      1-dim list, raw energy
+        pdos:      If spin-polarized: 
+                       [[spin up array],[spin down array]]
+                   If not spin-polarized:
+                       [pdos array]
     '''
+    engy = []
     pdos = []
-    print('initial pdos', pdos)
+    if spin_pol:
+        pdos.extend([[],[]])
     while True:
         line = inpf.readline()
         if not line:
@@ -24,17 +31,22 @@ def read_pdosf(inpf, spin_pol):
         if words[0]=='#':
             continue
         else:
+            engy.append( float(words[0]) )
             if not spin_pol:
                 pdos.append( float(words[1]) )
             elif spin_pol:
-                pdos.append( [float(words[1]),float(words[2])] )
-    return pdos
+                pdos[0].append( float(words[1]) )
+                pdos[1].append( float(words[2]) )
+    return engy, pdos
 
 
 
-pdos = read_pdosf(inf, True)
+engy, pdos = read_pdosf(inf, True)
 
 print(pdos)
+print(engy)
+
+sys.exit()
 
 
 
