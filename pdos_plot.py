bedbae7e2de5f1e061252e77ab7d6c38c8932ca2
anyonsites <xjin.xmu@gmail.com>
2015-05-18 17:49:58 +0800

    finish plot feature

--- pdos_plot.py
+++ pdos_plot.py
@@ -6,37 +6,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pdos_read import read_pdos
 
-workpath = '/home/jinxi/pwjobs/au_surface_proj/au111_lyr5/'
-os.chdir(workpath)
-filpdos_prefx = 'au111.pdos.plot'
-spin_pol = True
-sum_spin = True
-atom_list = list( range(1,6) )
-
 project_dict = { 'Au' : ['s','p','d'],
                  'H'  : ['s'],
                  'O'  : ['s','p'],
                }
+# Read surface
+workpath = '/home/jinxi/pwjobs/au_surface_proj/au111_lyr5/'
+os.chdir(workpath)
+filpdos_prefx = 'au111.pdos.plot'
+atom_list = list( range(1,6) )
+fermi = 2.8450
+atm_wfc_lst = []
+for id_atom in atom_list:
+    atm_wfc_lst.append( ( str(id_atom), 'Au', project_dict['Au'] ) )
+engy, pdos = read_pdos(filpdos_prefx, atm_wfc_lst, True, True)
+engy = [engy_val - fermi for engy_val in engy]
 
-atm_wfc_lst = [
-      ('2','Au',['s','p','d'] ),
-      ('4','Au',['s','p','d'] ),
-    ]
+# Read bulk
+workpath = '/home/jinxi/pwjobs/au_surface_proj/au_bulk/'
+os.chdir(workpath)
+filpdos_prefx = 'au.pdos.plot'
+fermi_bulk = 14.6937
+atm_wfc_lst = [ ( '1', 'Au', project_dict['Au'] ) ]
+engy_bulk, pdos_bulk = read_pdos(filpdos_prefx, atm_wfc_lst, True, True)
+engy_bulk = [engy_val - fermi_bulk for engy_val in engy_bulk]
 
-engy, pdos = read_pdos(filpdos_prefx, atm_wfc_lst, spin_pol, sum_spin)
 
-
-print(pdos)
-#print (engy)
 
 fig = plt.figure()
 plot_wfc_list = ['s','p','d']
 for wfc in plot_wfc_list:
-    ax = fig.add_subplot( 1, nwfc, (plot_wfc_list.index(wfc)+1) )
+    ax = fig.add_subplot( 1, len(plot_wfc_list), (plot_wfc_list.index(wfc)+1) )
     for (atom_id, atom, wfc_list) in pdos:
-        ax.plot
-
-
+        for wfc_lpdos in wfc_list:
+            if wfc_lpdos[0] == wfc:
+                ax.plot(wfc_lpdos[1], engy, label=atom_id)
+    for (atom_id, atom, wfc_list) in pdos_bulk:
+        for wfc_lpdos in wfc_list:
+            if wfc_lpdos[0] == wfc:
+                ax.plot(wfc_lpdos[1], engy_bulk, label='Bulk')
+    ax.legend(loc="lower right", title=wfc)
+plt.show()
 
 sys.exit()
+
 
