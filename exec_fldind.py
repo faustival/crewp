0b5cbca677e090f1a2c65e5f07a68c553dd02518
anyonsites <xjin.xmu@gmail.com>
2015-10-24 15:33:28 +0800

    iterating plot fields

--- exec_fldind.py
+++ exec_fldind.py
@@ -23,7 +23,7 @@ Data from class SlabFreeChrg:
                   ...
                  ]
 '''
-rootpath = '/home/jinxi/pwjobs/'
+rootpath = '/home/jinxi/pwjobs/chrgden_field'
 os.chdir(rootpath)
 
 bulklist = [
@@ -31,18 +31,18 @@ bulklist = [
         ]
 
 slablist = [
-  { 'elem':'Rb',  'ort':'111', 'bands':[10,5], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1],},
-  { 'elem':'Ag',  'ort':'111', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1],},
-#  { 'elem':'Ag', 'ort':'100', 'bands':[60,35], 'flds':[0.0, 0.1] , 'shift':0, 'slablmt':[0,-1],},
-#  { 'elem':'Ag', 'ort':'110', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1], },
+  { 'elem':'Rb', 'ort':'111', 'bands':[10,5],  'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1],},
+  { 'elem':'Ag', 'ort':'111', 'bands':[60,35], 'flds':[0.0, 0.01, 0.1], 'shift':0, 'slablmt':[0,-1],},
+  { 'elem':'Ag', 'ort':'100', 'bands':[60,35], 'flds':[0.0, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1], 'shift':0, 'slablmt':[0,-1],},
+  { 'elem':'Ag', 'ort':'110', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1], },
   { 'elem':'Pt', 'ort':'111', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1], },
   { 'elem':'Pt', 'ort':'100', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1], },
   { 'elem':'Pt', 'ort':'110', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1], },
   { 'elem':'Au', 'ort':'111', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1], },
   { 'elem':'Au', 'ort':'100', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1], },
   { 'elem':'Au', 'ort':'110', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':0, 'slablmt':[0,-1], },
-  { 'elem':'Ag15-', 'ort':'111', 'bands':[100,75], 'flds':[0.0, 0.1], 'shift':4, 'slablmt':[0,-1],},
-  { 'elem':'Ag21-', 'ort':'111', 'bands':[130,105], 'flds':[0.0, 0.1], 'shift':7, 'slablmt':[0,-1],},
+#  { 'elem':'Ag15-', 'ort':'111', 'bands':[100,75], 'flds':[0.0, 0.1], 'shift':4, 'slablmt':[0,-1],},
+#  { 'elem':'Ag21-', 'ort':'111', 'bands':[130,105], 'flds':[0.0, 0.1], 'shift':7, 'slablmt':[0,-1],},
 #  { 'elem':'Pt-H', 'ort':'111', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':1, 'slablmt':[0,-1], },
 #  { 'elem':'Pt-H-Sym', 'ort':'111', 'bands':[60,35], 'flds':[0.0, 0.1], 'shift':1, 'slablmt':[1,-1], },
 #  { 'elem':'Pt15-H-Sym', 'ort':'111', 'bands':[100,75], 'flds':[0.0, 0.1], 'shift':1, 'slablmt':[1,-2], },
@@ -50,8 +50,8 @@ slablist = [
 
 
 #readinlist = [ item for item in slablist if item['ort']=='111' ]
-readinlist = [item for item in slablist if item['elem'][0:2]=='Rb' ]
-plotxlim = [-40., 25.]
+readinlist = [item for item in slablist if (item['elem'][0:2]=='Ag' and item['ort']=='100') ]
+plotxlim = [0., 5.]
 colorlist = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
 labelfontsize = 30
 
@@ -133,13 +133,17 @@ for slab in slab_plotlist:
     if bulklist:
         ax_freeratio.plot(slab.zaxis, slab.freeratio[0], color=color, label=legend_pref+' Free, density ratio')
         ax_freeratio.plot(slab.zaxis, slab.freeratio[1], color=color, linestyle='--', dashes=(5,1.5), label=legend_pref+' Free, density ratio, field')
-    # 
-    ax_diff_tot.plot(slab.zaxis, -slab.chrgz_diffld[0][0], color=color, label=legend_pref+' Total')
-    ax_diff_free.plot(slab.zaxis, -slab.chrgz_diffld[0][1], color=color, linestyle='--', dashes=(5,1.5), label=legend_pref+' $d$')
-    ax_diff_free.plot(slab.zaxis, -slab.chrgz_diffld[0][2], color=color, label=legend_pref+' Free')
-    for ax in ax_diff:
-        for z0 in slab.imgplane:
+    # iterating plot over fields
+    for i in range(0, len(slab.flds)-1):
+        legend_pref = slab.elem + slab.ort + 'f={0:.2f}'.format(slab.flds[i+1])
+        ax_diff_tot.plot(slab.zaxis, -slab.chrgz_diffld[i][0], color=color, label=legend_pref+' Total')
+        ax_diff_free.plot(slab.zaxis, -slab.chrgz_diffld[i][1], color=color, linestyle='--', dashes=(5,1.5), label=legend_pref+' $d$')
+        ax_diff_free.plot(slab.zaxis, -slab.chrgz_diffld[i][2], color=color, label=legend_pref+' Free')
+        z0 = slab.imgplane[i]
+        for ax in ax_diff:
             ax.axvline(x=z0, color=color, linewidth=1, linestyle='--')
+        counter += 1
+        color = colorlist[counter % len(colorlist)]
 # all axes
 for ax in axlist:
     ax.legend(loc=1)
