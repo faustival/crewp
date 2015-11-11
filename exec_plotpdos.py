942e080be3b1077bfa1fe821cdab7944cabb3c9c
anyonsites <xjin.xmu@gmail.com>
2015-11-11 19:00:33 +0800

    finish plot pdos

--- exec_plotpdos.py
+++ exec_plotpdos.py
@@ -1,23 +1,41 @@
 #! /usr/bin/python3
 
 import os
+import matplotlib.pyplot as plt
 from atom import Atom
 
-slablist = { 'au3pd'   : [ {'atomid':12, 'elem':'Au', 'orbitals':['d','s'],},
-                           {'atomid':13, 'elem':'Pd', 'orbitals':['d'],},
-                         ],
-             'au4_top' : [ {'atomid':16, 'elem':'Au', 'orbitals':['d'],},
-                           {'atomid':17, 'elem':'C' , 'orbitals':['s','p'],},
-                         ],
+slabdict = {
+            'au3pd' :            { 'atmlist': [
+                                               {'atomid':15, 'elem':'Pd', 'orbitals':['d'],},
+                                              ],
+                                   'fermi': 0.9020,
+                                 },
+            'au3pd_step0604' :   { 'atmlist': [
+                                               {'atomid':33, 'elem':'Pd', 'orbitals':['d'],},
+                                               {'atomid':32, 'elem':'Pd', 'orbitals':['d'],},
+                                               {'atomid':31, 'elem':'Pd', 'orbitals':['d'],},
+                                              ],
+                                   'fermi': 0.4466,
+                                 },
            }
 
 rootdir = '/home/jinxi/pwjobs/cyan_ads/'
 
-for slabdir in slablist:
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+for slabdir in slabdict:
     os.chdir(rootdir + slabdir)
-    for atom in slablist[slabdir]:
+    fermi = slabdict[slabdir]['fermi']
+    for atom in slabdict[slabdir]['atmlist']:
         atm = Atom(atom['atomid'],atom['elem'])
         atm.get_ldos('plt', atom['orbitals'], readpdos=False, spin=False)
+        atom['ldos'] = atm.ldos
+        ldos = atom['ldos']
+        ax.plot( ldos['enary']-fermi, ldos['d'], label=(slabdir+', '+str(atom['atomid'])) )
+
+ax.legend()
+plt.show()
 
 
 
