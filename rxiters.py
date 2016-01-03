cc305d76e62a4f1d3bad0632f5bb34dbf1638b36
anyonsites <xjin.xmu@gmail.com>
2016-01-03 21:02:22 +0800

    class rxIters to monitor force and energy convergence in geometric relax

---
+++ rxiters.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+import re
+
+class rxIters:
+
+    def __init__(self, oupfname):
+        self.oupfname = oupfname
+
+    def rxconv(self):
+        '''
+        Reading total energies and force from
+        each cycles of geometric opt.
+        '''
+        self.enseq = []
+        self.forcseq = []
+        with open(self.oupfname, 'r') as oupf:
+            while True:
+                line = oupf.readline()
+                # search energy conv
+                rechk = re.search('!', line)
+                if rechk:
+                    words = line.split()
+                    self.enseq.append(float(words[4]))
+                rechk = re.search('Total force', line)
+                if rechk:
+                    words = line.split()
+                    self.forcseq.append(float(words[3]))
+                if not line:
+                    break
+        self.iterseq = [i for i in range(1, len(self.enseq)+1)]
+
+    def printconv(self):
+        print('{0:>10s} {1:>10s} {2:>18s}'.format('Iter', 'Force', 'Energy'))
+        for i in range(len(self.iterseq)):
+            print('{0:10d} {1:10.4f} {2:18.8f}'.format(self.iterseq[i], self.forcseq[i], self.enseq[i]))
+
+
