85fc9d3f54e50dc81891b6c6c3693c5723e8f092
anyonsites <xjin.xmu@gmail.com>
2016-08-09 11:50:05 +0800

    dig out element from POTCAR information in OUTCAR

---
+++ bin/outcar2axsf.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+import sys
+from qescripts.vasp.outcar import Outcar
+
+if len(sys.argv) < 2:
+    inpfname = 'OUTCAR'
+else:
+    inpfname = sys.argv[1]
+
+print('Reading VASP OUTCAR filetype, ', inpfname)
+
+outcar_obj = Outcar(inpfname)
+outcar_obj.get_basics()
--- vasp/outcar.py
+++ vasp/outcar.py
@@ -1,4 +1,45 @@
 
+import re
+
+def read_pos_vec():
+    '''
+    Read position-vector natom*(3+3) dimensional tuple,
+    used in:
+    * ionic relaxation: position(3) - force(3)  
+    * frequency: position(3) - eigenvector of dynamical matrix(3)
+    '''
+    pass
 
 class Outcar:
-    def __init__(self, inpf):
+    def __init__(self, fname):
+        self.fname = fname
+
+    def get_basics(self):
+        elements = []
+        outcarf = open(self.fname, 'r')
+        while True:
+            line = outcarf.readline()
+            # find all atomic types
+            if 'POTCAR' in line: 
+                source = line.split()[2]
+                m = re.match('[A-Z][a-z]?', source)
+                if m and m.group() not in elements:
+                    elements.append(m.group())
+                    print(m.group())
+                elif len(elements)>0 and m.group() in elements:
+                    pass
+            # how many atoms each type
+            if 'ions per type' in line: 
+                pass
+            if not line: break
+        outcarf.close()
+
+    def get_rx_force(self):
+        outcarf = open(self.fname, 'r')
+        while True:
+            line = outcarf.readline()
+            if not line: break
+        outcarf.close()
+
+
+
