13a08e2a9a9d7a0845f043f2cc90359585df0a85
anyonsites <xjin.xmu@gmail.com>
2017-02-06 15:58:50 +0800

    finish vaspxml position and force steps

--- vasp/parsexml.py
+++ vasp/parsexml.py
@@ -3,42 +3,78 @@
 import lxml.etree as etr
 import numpy as np
 from crewp.lattice.coordtrans import frac2cart
+import sys
 
-def type_step_arrays(arrayname, ary3):
+'''
+# delete these typing function after test 
+'''
+def type_varray(arrayname, ary2):
     '''
-    # delete this checking function after test 
+    Printing 2-index array, i.e., matrix
     '''
-    print("Printing array: ", arrayname, )
-    for i, coordstep in enumerate(ary3):
-        print("STEP ", i)
-        for coord in coordstep:
-            print( '   '+' '.join( '{:13.8f}'.format(entry) for entry in coord ) )
-        print('\n')
+    print("  Printing 2d-array: ", arrayname, )
+    for vec in ary2:
+        print( '   '+' '.join( '{:13.8f}'.format(entry) for entry in vec ) )
+    print('\n')
 
-        '''
-        pos_steps: atomic positions of each SCF step in fractional crystal coordinate, 
-                   shape( nsteps, natoms, 3)
-        latvec_steps: lattice cell vectors in each SCF step, 
-                   shape( nsteps, 3, 3)
-        '''
+def type_step_varrays(arrayname, ary3):
+    print("Printing arrays: ", arrayname, )
+    for i, ary2 in enumerate(ary3):
+        ary2dname = 'STEP: '+'{:d}'.format(i)
+        type_varray(ary2dname, ary2)
+
+'''
+latvec_init: initial lattice cell vectors, 
+            shape( 3, 3)
+======
+position_steps: atomic positions of each SCF step in fractional crystal coordinate, 
+            shape( nsteps, natoms, 3)
+latvec_steps: lattice cell vectors in each SCF step, 
+            shape( nsteps, 3, 3)
+force_steps: forces of each SCF step,
+            shape( nsteps, natoms, 3)
+'''
 xpath_dict = { 
-        'pos_steps' : '//calculation/structure/varray[@name="positions"]' ,  
+        'latvec_init' : '//structure[@name="initialpos"]/crystal/varray[@name="basis"]' ,
+        'position_steps' : '//calculation/structure/varray[@name="positions"]' ,  
         'latvec_steps' : '//calculation/structure/crystal/varray[@name="basis"]' ,
+        'force_steps' : '//calculation/varray[@name="forces"]' ,
         }
+
+def varray2darry(elem_varray):
+    '''
+    Arrange data in ``elem_varray`` element (e.g., <varray>)
+    into numpy 2d-array.
+    '''
+    varray = [] # len = nvectors
+    for elem_vec in elem_varray:
+        varray.append( [ float(a) for a in elem_vec.text.split() ] )
+    return np.array(varray)
+
+def get_varray(xpathcode, fname='vasprun.xml', ):
+    '''
+    Only for varray matching ``xpathcode`` ONCE
+    '''
+    xmltree = etr.parse(fname)
+    elem_varray = xmltree.xpath(xpathcode) # note to unpack 
+    if len(elem_varray) != 1:
+        sys.exit('From get_varray, xpathcode returns not ONLY ONE match!')
+    elem_varray, = elem_varray
+    varray = varray2darry(elem_varray)
+    return varray
 
 def get_varray_steps(xpathcode, fname='vasprun.xml', ):
     '''
-    parsing 2D-arrays in <varray> tag, 
-    arrays in each SCF step is stacked
+    Stack 2D-arrays in each SCF step, 
+    from parsing 2D-arrays in <varray> tag, 
+    returns :
     varray_steps: shape( nsteps, nvectors, vector_dim)
     '''
     xmltree = etr.parse(fname)
     elem_list = xmltree.xpath(xpathcode)
     varray_steps = [] # len = number of steps
     for elem_varray in elem_list :
-        varray = [] # len = nvectors
-        for elem in elem_varray:
-            varray.append( [ float(a) for a in elem.text.split() ] )
+        varray = varray2darry(elem_varray)
         varray_steps.append(varray)
     return np.array(varray_steps)
 
@@ -52,16 +88,20 @@ def auto_creep(fname='vasprun.xml'):
     elem_isif, = xmltree.xpath('//parameters/separator[@name="ionic"]/i[@name="ISIF"]') # don't parse <incar> tag
     ibrion = int(elem_ibrion.text)
     isif = int(elem_isif.text)
-    if 1 <= ibrion <= 3: # ionic relax
+    if -1 <= ibrion <= 3: # ionic steps
+        position_steps = get_varray_steps(xpath_dict['position_steps'], fname) # fractional coordinate
+        force_steps = get_varray_steps(xpath_dict['force_steps'], fname)
         if 0 <= isif <= 2 : # fixed cell
-            pos_steps = get_varray_steps(xpath_dict['pos_steps'], fname)
+            latvec_init = get_varray(xpath_dict['latvec_init'], fname)
+            position_steps_cart = []
+            for i in range(len(position_steps)):
+                position_steps_cart.append( frac2cart( position_steps[i], latvec_init ) )
         elif 3 <= isif <= 7 : # variable cell
-            pos_steps = get_varray_steps(xpath_dict['pos_steps'], fname)
             latvec_steps = get_varray_steps(xpath_dict['latvec_steps'], fname)
-            pos_steps_cart = []
-            for i in range(len(pos_steps)):
-                pos_steps_cart.append( frac2cart( pos_steps[i], latvec_steps[i] ) )
-            type_step_arrays('pos_steps_cart', pos_steps_cart) # print to check arrays
+            position_steps_cart = []
+            for i in range(len(position_steps)):
+                position_steps_cart.append( frac2cart( position_steps[i], latvec_steps[i] ) )
+        type_step_varrays('position_steps_cart', position_steps_cart) # print to check arrays
     elif 5 <= ibrion <= 8: # vibrational frequencies
         pass
     else:
