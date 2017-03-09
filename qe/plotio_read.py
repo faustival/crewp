613921c4897a148505bfcc79646b36f79208add4
anyonsites <xjin.xmu@gmail.com>
2017-03-09 22:40:40 +0800

    remove redundant things in qe plotio reading

--- qe/plotio_read.py
+++ qe/plotio_read.py
@@ -1,26 +1,5 @@
 
 import numpy as np
-import sys
-
-def get_words(inpf):
-    line = inpf.readline()
-    return line.split()
-
-def lines2array(inpf,ncol):
-    arry = []
-    while True:
-        words = get_words(inpf)
-        if len(words)>ncol:
-            print('RESET the column number!')
-            break
-        elif len(words)<ncol:
-            rowary = [float(num) for num in words]
-            arry += rowary
-            break
-        else:
-            rowary = [float(num) for num in words]
-            arry += rowary
-    return arry
 
 def plotio_read(inpfname):
     '''
@@ -34,28 +13,28 @@ def plotio_read(inpfname):
     '''
     inpf = open(inpfname, 'r')
     # title
-    words = get_words(inpf)
+    words = inpf.readline().split()
     # grid number, number of atom, number of atomic types
-    words = get_words(inpf)
+    words = inpf.readline().split()
     nr1x, nr2x, nr3x, nr1, nr2, nr3, nat, ntyp = [int(nu) for nu in words.copy()]
     # ibrav and celldm
-    words = get_words(inpf)
+    words = inpf.readline().split()
     ibrav = int(words[0])
     celldm = tuple([float(words[i]) for i in range(1,7)])
     if ibrav==0:
         cell = np.zeros((3,3))
         for i in range(3):
-            words = get_words(inpf)
+            words = inpf.readline().split()
             cellvec = np.array([float(nu) for nu in words])
             cell[i] = cellvec
     # energy cutoff, ...
-    words = get_words(inpf)
+    words = inpf.readline().split()
     gcutm, dual, ecut = [float(words[i]) for i in range(3)]
     plot_num = int(words[3])
     # atom type list and valence charge
     zvalence = []
     for i in range(ntyp):
-        words = get_words(inpf)
+        words = inpf.readline().split()
         zval = []
         zval.append(int(words[0]))
         zval.append(words[1])
@@ -66,20 +45,13 @@ def plotio_read(inpfname):
     # atomic positions
     atom_coord = np.zeros((nat,3))
     for i in range(nat):
-        words = get_words(inpf)
+        words = inpf.readline().split()
         atm = np.array([float(nu) for nu in words[1:4]])
         atom_coord[i] = atm
     # 3d array quantities, e.g. charge density, written as '(5(1pe17.9))'
-    ary1d = lines2array(inpf,5)
-    inpf.close()
-    ary1d = np.array(ary1d)
-    if (len(ary1d) != nr3*nr2x*nr1x) :
-        print('Error with 3-Dim array reading, length not match!')
-        print('len of 1-Dim array',len(ary1d))
-        sys.exit()
+    ary1d = np.fromfile(inpf, count=nr3*nr2x*nr1x, sep=' ')
     ary3d = ary1d.reshape(nr3, nr2x, nr1x)
-    print('len of 1-Dim array',len(ary1d))
-    print('shape of 3-Dim array',ary3d.shape) 
+    inpf.close()
     '''
     Convert cell-vectors and atomic positions to 
     atomic unit, Bohr, 3-Dim grid is set
@@ -89,7 +61,6 @@ def plotio_read(inpfname):
     cell = cell*alat
     atom_coord = atom_coord*alat
     return ary3d, cell, atom_coord
-
 
 class PlotIORead:
     '''
