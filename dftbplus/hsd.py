8fbaed9a98acdebbfe1657fe63b8719790fb56c0
anyonsites <xjin.xmu@gmail.com>
2017-07-08 21:26:08 +0800

    write dftbplus keyword_dict prototype

--- dftbplus/hsd.py
+++ dftbplus/hsd.py
@@ -2,9 +2,9 @@
 def read_hsd(fname = 'dftb_in.hsd'): 
     global curr_dict
     hsdf = open(fname, 'r')
-    inp = {}
+    keywords_dict = {}
     keypath = []
-    curr_dict = inp
+    curr_dict = keywords_dict
     def nest_keys(hsdf): # recursive build nested-key
         global curr_dict
         while True:
@@ -13,28 +13,23 @@ def read_hsd(fname = 'dftb_in.hsd'):
                 break
             elif '}' in line: # break current recurse, find parent dictionary
                 del keypath[-1]
-                curr_dict = dict_from_path(keypath, inp) # backward 1 depth
-                print( 'KEYPATH: ', keypath)
+                curr_dict = dict_from_path(keypath, keywords_dict) # backward 1 depth
                 break
-            elif line.strip() =='':
-                print(line[:-1] + 'Blank line: ', )
+            elif line.strip() =='': # Blank line
                 pass
             elif '{' in line: # meet recurse
-                print(line[:-1] + 'Beginning new dict: ', )
                 try: # key has attribute
                     key = line[0:line.index('=')].strip()
                 except ValueError: # key without attribute
                     key = line[0:line.index('{')].strip()
                 curr_dict[key] = {} # initialize new key:dictionary
                 keypath.append(key)
-                curr_dict = dict_from_path(keypath, inp) # forward 1 depth
-                print( 'KEYPATH: ', keypath)
+                curr_dict = dict_from_path(keypath, keywords_dict) # forward 1 depth
                 nest_keys(hsdf) 
-            else:
-                print(line[:-1] + '  Normal key: ', )
+            else: # Normal key 
+                pass
     nest_keys(hsdf)
-    print(inp)
-    return inp
+    return keywords_dict
 
 def dict_from_path(keypath, dictroot):
     d = dictroot
@@ -44,4 +39,10 @@ def dict_from_path(keypath, dictroot):
 
 special_keys = [ 'ProjectStates' ]
 
-
+def write_hsd(keywords_dict, hsdf, indent=0):
+    for key, value in keywords_dict.items():
+        print('\t'*indent + key)
+        if isinstance(value, dict):
+            write_hsd(value, hsdf, indent+1)
+        else:
+            print('\t'*indent + key+': '+str(value))
