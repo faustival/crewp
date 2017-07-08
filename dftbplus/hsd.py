b67838c0f757a73833e91d8a70a687efc3c8e5eb
anyonsites <xjin.xmu@gmail.com>
2017-07-08 16:54:32 +0800

    Build input key dictionary properly

--- dftbplus/hsd.py
+++ dftbplus/hsd.py
@@ -1,47 +1,47 @@
 
-
-
 def read_hsd(fname = 'dftb_in.hsd'): 
+    global curr_dict
     hsdf = open(fname, 'r')
     inp = {}
     keypath = []
     curr_dict = inp
     def nest_keys(hsdf): # recursive build nested-key
+        global curr_dict
         while True:
             line = hsdf.readline()
-            if not line: 
-                print('Meet EOF')
+            if not line: # Meet EOF
                 break
             elif '}' in line: # break current recurse, find parent dictionary
-                print(line[:-1] + 'End of current dict: ', )
-                #del keypath[-1]
+                del keypath[-1]
+                curr_dict = dict_from_path(keypath, inp) # backward 1 depth
+                print( 'KEYPATH: ', keypath)
                 break
             elif line.strip() =='':
                 print(line[:-1] + 'Blank line: ', )
+                pass
             elif '{' in line: # meet recurse
                 print(line[:-1] + 'Beginning new dict: ', )
-                #new_dict_line(line)
+                try: # key has attribute
+                    key = line[0:line.index('=')].strip()
+                except ValueError: # key without attribute
+                    key = line[0:line.index('{')].strip()
+                curr_dict[key] = {} # initialize new key:dictionary
+                keypath.append(key)
+                curr_dict = dict_from_path(keypath, inp) # forward 1 depth
+                print( 'KEYPATH: ', keypath)
                 nest_keys(hsdf) 
             else:
                 print(line[:-1] + '  Normal key: ', )
     nest_keys(hsdf)
+    print(inp)
     return inp
 
-def dict_from_path(keypath):
-    d = inp
+def dict_from_path(keypath, dictroot):
+    d = dictroot
     for key in keypath:
         d = d[key]
     return d
 
-def new_dict_line(line):
-    try: # key has 
-        key = line[0:line.index('=')].strip()
-    except ValueError:
-        key = line[0:line.index('{')].strip()
-    curr_dict[key] = {}
-    keypath.append(key)
-    curr_dict = dict_from_path(keypath)
-
-
+special_keys = [ 'ProjectStates' ]
 
 
