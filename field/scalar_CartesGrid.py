bb62b30f4ee7a528a1a93d5d6113f9b8972792c2
faustival <xjin.xmu@gmail.com>
2018-09-09 14:31:45 +0800

    Improve field:Scalar3d:averages, plot_cube

--- field/scalar_CartesGrid.py
+++ field/scalar_CartesGrid.py
@@ -7,6 +7,7 @@ from crewp.gaus_chem.cube import read_cube
 class ScalarField:
     '''
     Applies to e.g., charge-density, potentials, 
+    self.data3d should by passed in with axes ordered as 'x-y-z'
     '''
     def __init__( 
             self, 
@@ -35,7 +36,7 @@ class ScalarField:
                 self.delta_grid = np.diagonal(self.grid_measure)
         self.gridshape = self.data3d.shape
 
-    def get_grid_arrys(self):
+    def set_grid_arrys(self):
         '''
         Calculate grid arrays from increaments ``delta_grid``
         Return a list of grid arrays, length may not be equal.
@@ -51,19 +52,37 @@ class ScalarField:
             l0 = self.cell_origin[i]
             grid_arry = l0 + dl*np.arange(ng) 
             self.grid_arry.append(grid_arry)
-        return self.grid_arry
 
-    def get_avg2d(self, axis):
+    def get_avg2d(self, axes):
         '''
-        Average through axis.
+        Average 3d data to 2d axes.
+        axes = 'xy'|'yx'|'xz'|'zx'|'yz'|'zy'
         '''
-        avg2d = np.mean(self.data3d, axis=axis)
-        return avg2d
+        axes_return = [ {'x':0,'y':1,'z':2}[char] for char in axes ]
+        axis_mean, = set([0,1,2]) - set(axes_return)
+        avg2d = np.mean(self.data3d, axis=axis_mean)
+        if axes_return[0] > axes_return[1]: 
+            # input axes ordered like 'zy','yx','zx'
+            avg2d = np.transpose(avg2d)
+        if not hasattr(self, 'grid_arry'): 
+            self.set_grid_arrys()
+        return avg2d, [ self.grid_arry[i] for i in axes_return ]
 
-    def get_avg1d(self, axes):
+    def get_avg1d(self, axis):
         '''
-        Average through axes.
+        Average 3d data to 1d axis.
+        axis = 'x'|'y'|'z'
         '''
-        avg2d = np.mean(self.data3d, axis=max(axes))
-        avg1d = np.mean(avg2d, axis=min(axes))
-        return avg1d
+        axis_return = {'x':0,'y':1,'z':2}[axis]
+        axes_mean = [ n for n in [0,1,2] if n != axis_return ]
+        avg2d = np.mean(self.data3d, axis=max(axes_mean))
+        avg1d = np.mean(avg2d, axis=min(axes_mean))
+        if not hasattr(self, 'grid_arry'): 
+            self.set_grid_arrys()
+        return avg1d, self.grid_arry[axis_return]
+
+    def auto_avg(self, axs):
+        if len(axs)==1:
+            self.get_avg2d(axs)
+        elif len(axs)==2:
+            self.get_avg1d(*axs)
